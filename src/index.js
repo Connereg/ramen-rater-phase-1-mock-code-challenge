335d645f27853eecea3ae6a3d6183322d2762d58
Conner Gleason <connereg@optonline.net>
2021-08-21 10:51:13 -0400

    First three Deliverables

--- src/index.js
+++ src/index.js
@@ -1,58 +1,78 @@
+const BASE_URL_RAMEN_RESOURCES = "http://localhost:3000/ramens"
+
 // write your code here
-fetch(' http://localhost:3000/ramens')
-    .then(r => r.json())
-    .then(arr => {
-        arr.forEach(ramen => {
-            renderNewRamen(ramen)
-        });
+//SCRIPT SRC DEFERS
+function fetchingRamenResources(){
+    fetch(BASE_URL_RAMEN_RESOURCES)
+    .then((resp) => resp.json())
+    .then(rawData => rawData.forEach(eachRmn => renderRamenDb(eachRmn)))
+    //Raw Data Returns as an Array of Objects
+}
+//DELIVERABLE 1 & 2
+function renderRamenDb(thisRamen){
+    const ramenTopBarLocale = document.getElementById('ramen-menu')
+    const ramenImg = document.createElement('img')
+
+    ramenImg.src = thisRamen.image
+    ramenImg.addEventListener('click', (e) => {
+        const ramenCenterImg = document.querySelector("#ramen-detail > img")
+        const ramenCenterName = document.querySelector("#ramen-detail > h2")
+        const ramenCenterRestaurant = document.querySelector("#ramen-detail > h3")
+        const ramenRatingField = document.querySelector("body > p:nth-child(5) > span")
+        const ramenCommentField = document.querySelector("body > p:nth-child(7)")
+
+        ramenCenterImg.src = thisRamen.image
+        ramenCenterName.textContent = thisRamen.name
+        ramenCenterRestaurant.textContent = thisRamen.restaurant
+        ramenRatingField.textContent = thisRamen.rating
+        ramenCommentField.textContent = thisRamen.comment
     })
 
+    ramenTopBarLocale.appendChild(ramenImg)
+}
 
-function renderNewRamen(ramen) {
-    const img = document.createElement('img')
-    img.src = ramen.image
-    document.querySelector('#ramen-menu').append(img)
-
-    img.addEventListener('click', e => {
-        const ramenDetail = document.querySelector("#ramen-detail")
-        const detailImg = ramenDetail.querySelector('.detail-image')
-        detailImg.src = ramen.image
-
-        const h2 = ramenDetail.querySelector('h2.name')
-        h2.textContent = ramen.name
-
-        const h3 = ramenDetail.querySelector('h3.restaurant')
-        h3.textContent = ramen.restaurant
-
-        const rating = document.querySelector("body > p:nth-child(5) > span")
-        rating.textContent = ramen.rating
-
-        const comment = document.querySelector("body > p:nth-child(7)")
-        comment.textContent = ramen.comment
+//DELIVERABLE 3
+function ramenImportFormFunctionality(){
+    const fullRamenForm = document.getElementById('new-ramen')
+    /*
+    const ramenNameInputBox = document.querySelector("#new-name")
+    const ramenRestaurantInputBox = document.querySelector("#new-restaurant")
+    const ramenImgInputBox = document.querySelector("#new-restaurant")
+    const ramenRatingInputBox = document.querySelector("#new-rating")
+    const ramenCommentInputBox = document.querySelector("#new-comment")
+    */
+    fullRamenForm.addEventListener('submit', (s) => {
+        s.preventDefault();
+        let newRamenSubmit = {
+            comment: s.target["new-comment"].value,
+            image: s.target.image.value,
+            name: s.target.name.value,
+            rating: s.target.rating.value,
+            restaurant: s.target.restaurant.value
+        }
+        renderRamenDb(newRamenSubmit)
+        fullRamenForm.reset();
     })
 }
-// document.querySelector('#ramen-menu').addEventListener('click', e => {
-//     if(e.target.matches('')) {
-
-//     }
-// })
 
 
 
 
-const form = document.querySelector("#new-ramen")
-form.addEventListener('submit', e => {
-    e.preventDefault()
-    const name = e.target[0].value
-    const restaurant = e.target[1].value
-    const image = e.target[2].value
-    const rating = e.target[3].value
-    const comment = e.target[4].value
-
-    const ramen = { name, restaurant, image, rating, comment }
-    console.log(ramen)
-    renderNewRamen(ramen)
-    e.target.reset()
-})
+//CALLED FUNCTIONS
+fetchingRamenResources();
+ramenImportFormFunctionality();
 
 
+
+/*
+EXAMPLE RAMEN OBJ:
+
+0:
+comment: "Delish. Can't go wrong with a classic!"
+id: 1
+image: "./assets/ramen/shoyu.jpg"
+name: "Shoyu Ramen"
+rating: 7
+restaurant: "Nonono"
+
+*/
